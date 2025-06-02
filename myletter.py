76f5bdb4c12cbe465122038788e26f063510d88a
itc-s25008 <s25008@std.it-college.ac.jp>
2025-06-02 07:26:19 +0000

    250602

---
+++ myletter.py
@@ -0,0 +1,15 @@
+spaces = ' '*25
+print('%s 12 Butts wynd' % spaces)
+print('%s Twinklebottom Heath' % spaces)
+print('%s West Snoring' % spaces)
+print()
+print()
+print('Dear Sir')
+print()
+print('I wish to report thattiles are missing from the')
+print('outside toilet roof.')
+print('I think it was bad wind the other night that blew them awy.')
+print()
+print('Regards')
+print('Malcolm Dithering')
+
