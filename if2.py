27b92c904d262ab3bd67d42295e6d4c4b325394c
itc-s25008 <s25008@std.it-college.ac.jp>
2025-06-16 07:55:24 +0000

    250616

---
+++ if2.py
@@ -0,0 +1,7 @@
+score=60
+
+if score >= 80:
+    print("やったね！次もこの調子だ")
+else:
+    print("残念でした")
+
---
+++ var9.py
@@ -0,0 +1,6 @@
+# b = "こんにちは"
+b = "100"
+if b.isdigit():
+    print(int(b)+23)
+else:
+    print("数値じゃないよ")
