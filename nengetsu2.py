5bbe02d35e318ff234ff2f36794cc9a1d150949d
itc-s25008 <s25008@std.it-college.ac.jp>
2025-06-30 07:08:56 +0000

    250630

--- nengetsu2.py
+++ nengetsu2.py
@@ -1,7 +1,9 @@
 
 import calendar
-year = int(input("今日は何年？"))
-mon = int(input("今日は何月？"))
-
-print(calendar.month(year,mon))
+def calendar2():
+    year = int(input("今日は何年？"))
+    mon = int(input("今日は何月？"))
+    return(calendar.month(year,mon))
+a = calendar2()
+print(a)
 
