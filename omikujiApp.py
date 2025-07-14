b2b34eddda322ffffa8e80324aab50ecda65f0ae
itc-s25008 <s2008@std.it-college.ac.jp>
2025-07-14 16:33:48 +0900

    250714

--- omikujiApp.py
+++ omikujiApp.py
@@ -1,14 +1,19 @@
 import tkinter as tk
 import random
+result = []
 def dispLabel():
     kuji = ["大吉","中吉","小吉","吉","末吉","凶","大凶"]
     lbl.configure(text=random.choice(kuji))
+    result.append(lbl.pack())
+    lbl2.configure(text=result)
 root = tk.Tk()
 root.geometry("200x100")
 
 lbl = tk.Label(text="おみくじアプリ")
 btn = tk.Button(text="おみくじ！", command = dispLabel)
+lbl2 = tk.Label(text="")
 
 lbl.pack()
 btn.pack()
+lbl2.pack()
 tk.mainloop()
